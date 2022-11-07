3f1758435d4c332af112b705c30b20f5a43bc02c
ziyi <sheeploveart@gmail.com>
2022-11-07 17:59:16 +0800

    <Dev> Implement UI

---
+++ src/App.js
@@ -0,0 +1,34 @@
+const App = () => {
+  return (
+      <div className="categories-container">
+        <div className="category-container">
+          {/*<img/>*/}
+          <h1>Hats</h1>
+          <p>shop now</p>
+        </div>
+        <div className="category-container">
+          {/*<img/>*/}
+          <h1>Jackets</h1>
+          <p>shop now</p>
+        </div>
+        <div className="category-container">
+          {/*<img/>*/}
+          <h1>Sneakers</h1>
+          <p>shop now</p>
+        </div>
+        <div className="category-container">
+          {/*<img/>*/}
+          <h1>Woman's</h1>
+          <p>shop now</p>
+        </div>
+        <div className="category-container">
+          {/*<img/>*/}
+          <h1>Man's</h1>
+          <p>shop now</p>
+        </div>
+
+      </div>
+  );
+};
+
+export default App;
