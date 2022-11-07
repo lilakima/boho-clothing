d63167581ac317530d12ca364ebdfc2a9c874738
ziyi <sheeploveart@gmail.com>
2022-11-07 18:29:28 +0800

    <Dev> set up categories

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 const App = () => {
-  return (
-      <div className="categories-container">
-        <div className="category-container">
-          {/*<img/>*/}
-          <h1>Hats</h1>
-          <p>shop now</p>
+    const categories = [
+        { id: 1, title: "Hats" },
+        { id: 2, title: "Jackets" },
+        { id: 3, title: "Sneakers" },
+        { id: 4, title: "Woman's" },
+        { id: 5, title: "Man's" }
+    ]
+    return (
+        <div className="categories-container">
+            {
+                categories
+                    .map((category) => {
+                        return <div key={category.id}
+                                    className="category-container">
+                            <div className="background-image" />
+                            <h1>{category.title}</h1>
+                            <p>shop now</p>
+                        </div>
+                    })
+            }
         </div>
-        <div className="category-container">
-          {/*<img/>*/}
-          <h1>Jackets</h1>
-          <p>shop now</p>
-        </div>
-        <div className="category-container">
-          {/*<img/>*/}
-          <h1>Sneakers</h1>
-          <p>shop now</p>
-        </div>
-        <div className="category-container">
-          {/*<img/>*/}
-          <h1>Woman's</h1>
-          <p>shop now</p>
-        </div>
-        <div className="category-container">
-          {/*<img/>*/}
-          <h1>Man's</h1>
-          <p>shop now</p>
-        </div>
+    )
+}
 
-      </div>
-  );
-};
-
-export default App;
+export default App
