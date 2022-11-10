291dce89e917c7c680ffd7aa35a8fd3ecef45a56
ziyi <sheeploveart@gmail.com>
2022-11-10 16:07:14 +0800

    <Dev> Adding sign in component as well as firebase and brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -27,4 +27,21 @@ export const db = getFirestore()
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid)
     const userSnapShot = await getDoc(userDocRef)
+
+    if (!userSnapShot.exists()) {
+        const {displayName, email} = userAuth
+        const createAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message)
+        }
+    }
+
+    return userDocRef
 }
